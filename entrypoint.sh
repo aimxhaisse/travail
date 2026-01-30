57197420d16ca5bc068c0f52016d70b3ce4d626e
s. rannou <mxs@sbrk.org>
2026-01-30 15:38:56 +0100

    feat: better handling of project/features directories

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/env zsh
 
+if [ -n "$TRAVAIL_IS_BASE" ]; then
+    echo "\033[1;33mWarning:\033[0m Entering 'base' directory, not a feature"
+fi
+
 if [ -f /workspace/.tool-versions ]; then
     mise install
 fi
