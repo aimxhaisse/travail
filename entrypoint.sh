6728b6e3e1e09f9ff85dc146909438fda2288056
s. rannou <mxs@sbrk.org>
2026-02-06 09:16:06 +0100

    feat: try interactive

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,5 @@
-#!/bin/env zsh
+#!/bin/zsh -i
 
-if [ -n "$TRAVAIL_IS_BASE" ]; then
-    echo "\033[1;33mWarning:\033[0m Entering 'base' directory, not a feature"
-fi
-
-if [ -f /workspace/.tool-versions ]; then
-    $HOME/.local/bin/mise install
-fi
+mise install
 
 exec $@
